9eaddaa0c9a461c7502df9bf541b9ecc03436634
Nathan Rajlich <nathan@tootallnate.net>
2017-12-12 12:50:18 -0800

    serve with charset=utf8

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { readFileSync } = require('fs');
 const installScript = readFileSync(join(__dirname, 'install.sh'), 'utf8');
 
 module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain; charset=utf8');
   const { pathname } = parse(req.url);
   const version = pathname.substr(1);
   if (version) {
