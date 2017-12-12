032925738ca7d6df2d743841dae6c42f13f0b411
Nathan Rajlich <nathan@tootallnate.net>
2017-12-12 12:36:47 -0800

    add pathname version

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const { join } = require('path');
+const { parse } = require('url');
 const { readFileSync } = require('fs');
 
 const installScript = readFileSync(join(__dirname, 'install.sh'), 'utf8');
 
 module.exports = async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  return installScript;
+  const { pathname } = parse(req.url);
+  const version = pathname.substr(1);
+  if (version) {
+    return installScript.replace('VERSION=latest', `VERSION=${version}`);
+  } else {
+    return installScript;
+  }
 };
